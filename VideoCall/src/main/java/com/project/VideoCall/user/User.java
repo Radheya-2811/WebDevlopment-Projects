37d6e0bf486be30e0ef98293b6321645e8a0a5fc
Radheya-2811 <dubeyh1414@gmail.com>
2024-07-02 17:57:37 +0530

    secret

--- VideoCall/src/main/java/com/project/VideoCall/user/User.java
+++ VideoCall/src/main/java/com/project/VideoCall/user/User.java
@@ -1,12 +1,19 @@
 package com.project.VideoCall.user;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 public class User {
+
+
     private String username;
     private String email;
     private String password;
