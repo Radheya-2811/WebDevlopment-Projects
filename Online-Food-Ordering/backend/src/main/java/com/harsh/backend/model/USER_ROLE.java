929b72ab0bd39ab390ed2491ee4f3fd7933ff8dc
Radheya-2811 <dubeyh1414@gmail.com>
2025-03-25 01:17:06 +0530

    models

---
+++ Online-Food-Ordering/backend/src/main/java/com/harsh/backend/model/USER_ROLE.java
@@ -0,0 +1,9 @@
+package com.harsh.backend.model;
+
+public enum USER_ROLE {
+    ROLE_CUSTOME,
+
+    ROLE_RESTAURANT_OWNE,
+
+    ROLE_ADMIN
+}
--- Online-Food-Ordering/backend/src/main/java/com/harsh/backend/model/User.java
+++ Online-Food-Ordering/backend/src/main/java/com/harsh/backend/model/User.java
@@ -1,5 +1,25 @@
 package com.harsh.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String fullname;
+    private String email;
+    private String password;
+    private USER_ROLE  role;
+
+
+    private list<Order> orders=new ArrayList<Order>();
+
 }
