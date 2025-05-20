20719e7dca0b755872cb6b8d2aa60b4fe9ea0395
Radheya-2811 <dubeyh1414@gmail.com>
2025-05-20 22:57:21 +0530

    food delivery project update

--- Online-Food-Ordering/backend/src/main/java/com/harsh/backend/model/User.java
+++ Online-Food-Ordering/backend/src/main/java/com/harsh/backend/model/User.java
@@ -1,17 +1,24 @@
 package com.harsh.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String fullname;
@@ -19,7 +26,8 @@ public class User {
     private String password;
     private USER_ROLE  role;
 
-
-    private list<Order> orders=new ArrayList<Order>();
+    @JsonIgnore
+    @OneToMany
+    private List<Order> orders=new ArrayList<>();
 
 }
