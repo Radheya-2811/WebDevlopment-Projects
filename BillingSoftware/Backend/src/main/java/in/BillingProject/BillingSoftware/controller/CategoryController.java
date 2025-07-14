8736455cd6c3e6e0db625ba99ec09948ccd23ee1
Radheya-2811 <dubeyh1414@gmail.com>
2025-07-15 01:07:08 +0530

    updated

--- BillingSoftware/Backend/src/main/java/in/BillingProject/BillingSoftware/controller/CategoryController.java
+++ BillingSoftware/Backend/src/main/java/in/BillingProject/BillingSoftware/controller/CategoryController.java
@@ -2,21 +2,27 @@ package in.BillingProject.BillingSoftware.controller;
 
 import in.BillingProject.BillingSoftware.io.CategoryRequest;
 import in.BillingProject.BillingSoftware.io.CategoryResponse;
+import in.BillingProject.BillingSoftware.service.CategoryService;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 
 
 @RestController
 @RequestMapping("/categories")
+@RequiredArgsConstructor
 public class CategoryController {
-   // @PostMapping
-  //  public CategoryResponse addCategory(@RequestBody CategoryRequest request) {
 
-  //  }
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public CategoryResponse addCategory(@RequestBody CategoryRequest request) {
+        return  categoryService.add(request);
+    }
 }
