a84f0a0ab74e63e20673a2ba2947e6650f3be5cf
Radheya-2811 <dubeyh1414@gmail.com>
2025-07-30 02:43:14 +0530

    read category

--- BillingSoftware/Backend/src/main/java/in/BillingProject/BillingSoftware/service/implementation/CategoryServiceImpl.java
+++ BillingSoftware/Backend/src/main/java/in/BillingProject/BillingSoftware/service/implementation/CategoryServiceImpl.java
@@ -8,7 +8,9 @@ import in.BillingProject.BillingSoftware.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +41,12 @@ public class CategoryServiceImpl implements CategoryService {
                 .createdAt(newCategory.getCreatedAt())
                 .updatedAt(newCategory.getUpdatedAt())
                 .build();
+    }
+    @Override
+    public List<CategoryResponse> read(){
+        return categoryRepository.findAll()
+                .stream()
+                .map(categoryEntity -> convertToResponse(categoryEntity))
+                .collect(Collectors.toList());
     }
 }
