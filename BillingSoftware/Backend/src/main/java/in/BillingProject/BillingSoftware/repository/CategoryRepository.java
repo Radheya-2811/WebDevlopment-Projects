03a70df84f82c64710045a9762c5d65841a4edc3
Radheya-2811 <dubeyh1414@gmail.com>
2025-08-02 18:57:47 +0530

    s3 bucket added

--- BillingSoftware/Backend/src/main/java/in/BillingProject/BillingSoftware/repository/CategoryRepository.java
+++ BillingSoftware/Backend/src/main/java/in/BillingProject/BillingSoftware/repository/CategoryRepository.java
@@ -3,6 +3,9 @@ package in.BillingProject.BillingSoftware.repository;
 import in.BillingProject.BillingSoftware.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Long> {
+     Optional<CategoryEntity>findByCategoryId(String categoryId);
 
 }
