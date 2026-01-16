f729b1b40467330dd84ffc3f21aa37244746aa72
Radheya-2811 <dubeyh1414@gmail.com>
2026-01-17 01:45:10 +0530

    updated

--- BillingSoftware/Frontend/client/src/App.jsx
+++ BillingSoftware/Frontend/client/src/App.jsx
@@ -12,11 +12,6 @@ const App=()=>{
     <div>
       <Menubar/>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/category" element={<ManageCategory/>}/>
-        <Route path="/users" element={<ManageUsers/>}/>
-        <Route path="/items" element={<ManageItems/>}/>
-        <Route path="/explore" element={<Explore/>}/>
         <Route path="/" element={<Dashboard/>}/>
       </Routes>
     </div>
