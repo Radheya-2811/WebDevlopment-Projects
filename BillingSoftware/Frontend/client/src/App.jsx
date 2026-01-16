0f3d0b77de6bd305de3ecb709e5a13191283bce0
Radheya-2811 <dubeyh1414@gmail.com>
2026-01-17 01:45:41 +0530

    login window added

--- BillingSoftware/Frontend/client/src/App.jsx
+++ BillingSoftware/Frontend/client/src/App.jsx
@@ -12,6 +12,11 @@ const App=()=>{
     <div>
       <Menubar/>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/category" element={<ManageCategory/>}/>
+        <Route path="/users" element={<ManageUsers/>}/>
+        <Route path="/items" element={<ManageItems/>}/>
+        <Route path="/explore" element={<Explore/>}/>
         <Route path="/" element={<Dashboard/>}/>
       </Routes>
     </div>
