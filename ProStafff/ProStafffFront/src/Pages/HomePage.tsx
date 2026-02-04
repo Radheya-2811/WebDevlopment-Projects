730ac153e1744526d716b88157984250b67194c8
Radheya-2811 <dubeyh1414@gmail.com>
2026-02-04 16:44:55 +0530

    new navlinks

--- ProStafff/ProStafffFront/src/Pages/HomePage.tsx
+++ ProStafff/ProStafffFront/src/Pages/HomePage.tsx
@@ -16,11 +16,14 @@ const HomePage = () => {
         {/* Overlay */}
         <div className="min-h-screen bg-black/60 flex items-center justify-center">
           <div className="text-center px-6">
-            <h1 className="text-xl md:text-6xl font-bold text-white mb-60">
-              <span className="text-[#1b3474]">ProStafff</span>-boutique luxury retail HR consultancy
+            <h1 className="text-xl md:text-6xl font-bold text-white mb-10 mt-0">
+              <span className="text-[#1b3474]">ProStafff</span>-retail HR consultancy
             </h1>
             <p className="max-w-3xl mx-auto text-lg md:text-xl text-gray-200 leading-relaxed">
-              
+              We specialise exclusively in luxury and premium retail recruitment, where brand representation, 
+              client experience, and talent quality are critical. Our approach is discreet, detail-driven, 
+              and highly selective ensuring every professional we present aligns with the brand's identity, 
+              service standards, and long-term vision.
             </p>
           </div>
         </div>
